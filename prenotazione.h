eecad250019d0b944bdd9e5cfeb16bb5ff781ad7
iscaldaferri1 <i.scaldaferri1@studenti.unisa.it>
2026-05-09 20:15:03 +0200

    Module "prenotazione.h" updated

--- prenotazione.h
+++ prenotazione.h
@@ -10,9 +10,9 @@ void distruggi_prenotazione(Prenotazione p); //distruttore
 
 //funzioni setter per modificare la prenotazione
 
-void prenotazione_set_posto(Prenotazione p, int posto); 
+void set_posto_prenotazione(Prenotazione p, int posto); 
 void annulla_prenotazione(Prenotazione p);
-void prenotazione_set_stato(Prenotazione p, StatoPrenotazione stato);
+void set_stato_prenotazione(Prenotazione p, StatoPrenotazione stato);
 
 //funzioni getter per accedere ai dati della prenotazione
 
@@ -20,8 +20,6 @@ int prenotazione_get_posto(const Prenotazione p);
 char* prenotazione_get_matricola(const Prenotazione p);
 Data prenotazione_get_data(const Prenotazione p);
 FasciaOraria prenotazione_get_fascia(const Prenotazione p);
-int prenotazione_get_check_in(const Prenotazione p);
-int prenotazione_get_check_out(const Prenotazione p);
 StatoPrenotazione prenotazione_get_stato(const Prenotazione p);
 
 
