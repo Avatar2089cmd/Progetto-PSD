6311f45c456243e6ffaa8f1d86c21247dc94ce79
Avatar <simone.ranucci1@gmail.com>
2026-05-12 15:42:52 +0200

    updating "queue.c" developping method described in "queue.h"

--- queue.c
+++ queue.c
@@ -1,29 +1,87 @@
 #include "queue.h"
+#include "shared.h"
+
+typedef struct QueueNode {
+    char matricola[MAX_MATRICOLA];
+    FasciaOraria fascia;
+    TipoAccesso tipo;
+    struct QueueNode *next;
+} QueueNode;
+
+typedef struct Queue {
+    QueueNode *head;
+    QueueNode *tail;
+    int size;
+} Queue;
+
+
+Queue crea_queue(Queue *q){
+    q = malloc(sizeof(struct Queue));
+    if(*q==NULL) return NULL; //gestione errore allocazione
+
+    q->head = NULL; //inizializzazione puntatore alla testa della coda
+    q->tail = NULL; //inizializzazione puntatore alla fine  della coda
+    q->size = 0; //inizializzazione della dimensione della coda
+
+    return *q;
+}
+
+void distruggi_queue(Queue q){
+    if(q == NULL) return;
+    QueueNode *current = q->head;
+    QueueNode *prox = NULL;
+    while(current != NULL){
+        prox = current->next;
+        free(current);
+        current = prox;
+    }
+    free(q);
+}
+
+// Implementazione delle funzioni dichiarate in queue.h
+
+int is_empty(Queue q){return q->size == 0;}
+int get_dimensione(Queue q){return q->size;}
+
+FasciaOraria queue_peek_fascia(Queue q){
+    if(is_empty(q)) return -1; //gestione coda vuota
+    return q->head->fascia;
+}
+char* queue_peek_matricola(Queue q){
+    if(is_empty(q)) return NULL; //gestione coda vuota
+    return q->head->matricola;
+}
+TipoAccesso queue_peek_tipo(Queue q){
+    if(is_empty(q)) return -1; //gestione coda vuota
+    return q->head->tipo;
+}    
 
 void enqueue(Queue q, const char *matricola, FasciaOraria fascia, TipoAccesso tipo){
     // Implementazione dell'aggiunta di una prenotazione alla coda
+    QueueNode *new_node = malloc(sizeof(QueueNode));
+    if (new_node == NULL) return; //gestione errore allocazione
+        strncpy(new_node->matricola, matricola, MAX_MATRICOLA);
+        new_node -> matricola[MAX_MATRICOLA -1] = '\0';
+        new_node -> fascia = fascia;
+        new_node -> tipo = tipo;
+        new_node -> next = NULL; 
+        if (is_empty(q)) {
+            q->head = new_node;
+            q->tail = new_node;
+        } else {
+            q->tail->next = new_node;
+            q->tail = new_node;
+        }
+        q->size++;
 }
 
 void dequeue(Queue q){
-    // Implementazione della rimozione della prenotazione in testa alla coda
-}
-
-int is_empty(Queue q){
-    // Implementazione del controllo se la coda è vuota
-}
-
-int get_dimensione(Queue q){
-    // Implementazione del conteggio del numero di studenti in coda di attesa
-}
-
-char* queue_peek_matricola(Queue q){
-    // Implementazione del getter per la matricola in testa alla coda
-}
-
-FasciaOraria queue_peek_fascia(Queue q){
-    // Implementazione del getter per la fascia oraria in testa alla coda
-}
-
-TipoAccesso queue_peek_tipo(Queue q){
-    // Implementazione del getter per il tipo di accesso in testa alla coda
+    if(is_empty(q)) return;
+    QueueNode *temp = q->head;
+    q->head = q->head->next;
+    if(q->head == NULL){
+        q->tail = NULL;
+    }
+    free(temp);
+    q->size--;
 }
