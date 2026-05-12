e0287ce029c2e340ce52e854e553f3e890305720
iscaldaferri1 <i.scaldaferri1@studenti.unisa.it>
2026-05-12 14:05:51 +0200

    upgrading functionality of "aula.c"

--- aula.c
+++ aula.c
@@ -16,7 +16,8 @@ int get_posti_liberi_aula(FasciaOraria fascia){
 }
 
 int blocca_posto_aula(FasciaOraria fascia){
-    for(int i = 0; i < POSTI_PER_FASCIA; i++){
+    int i=0;
+    for(i = 0; i < POSTI_PER_FASCIA; i++){
         if(aula[fascia][i] == 'L'){
             aula[fascia][i] = 'O'; //blocca il posto
             return i; //restituisce l'indice del posto bloccato
